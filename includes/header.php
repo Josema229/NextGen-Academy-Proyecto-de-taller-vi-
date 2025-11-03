e6c27b62affa7fa379f118cc221c70b25948bd46
Jose Alberto Martinez <josealbertoma229@gmail.com>
2025-11-03 20:40:47 -0300

    Proyecto de Taller VI

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>TechLearn - Academia de Informática</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand d-flex align-items-center" href="index.php">
+        <img src="img/logo.png" alt="TechLearn" width="40" class="me-2">
+        <span>TechLearn</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="menu">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>
+          <li class="nav-item"><a class="nav-link" href="nosotros.php">Nosotros</a></li>
+          <li class="nav-item"><a class="nav-link" href="servicios.php">Servicios</a></li>
+          <li class="nav-item"><a class="nav-link" href="empresa.php">La Empresa</a></li>
+          <li class="nav-item"><a class="nav-link" href="valores.php">Valores</a></li>
+          <li class="nav-item"><a class="nav-link" href="contacto.php">Contacto</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <main class="container my-5">
